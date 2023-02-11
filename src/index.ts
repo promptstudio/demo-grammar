48dc34f76dc91fff93a4cd3265c75379d58ab2c6
Valentin Golev <v.golev@gmail.com>
2023-02-11 04:43:20 +0100

    basic example

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
+import HTML from './index.html';
+
 export default {
 	async fetch(request: Request) {
+		let url = new URL(request.url);
+		let path = url.pathname.slice(1).split('/');
+
+		if (!path[0]) {
+			// Serve our HTML at the root path.
+			return new Response(HTML, { headers: { 'Content-Type': 'text/html;charset=UTF-8' } });
+		}
+
 		return new Response(`request method: ${request.method}`);
 	},
 };
